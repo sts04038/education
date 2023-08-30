ddf7d7e5fc237b6fde1b1d6d93dc89f4bd281c7d
Dong Hyuk Park <sts04038@gmail.com>
2023-08-30 11:16:15 +0900

    django-braces 설정

--- courses/admin.py
+++ courses/admin.py
@@ -16,7 +16,7 @@ class ModuleInline(admin.StackedInline):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['title', 'subject', 'created']
+    list_display = ['title', 'owner', 'subject', 'created']
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title',)}
